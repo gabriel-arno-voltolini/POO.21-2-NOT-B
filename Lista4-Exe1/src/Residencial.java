2c41874951a51eedd2369ab48f7e75c8daf9e43e
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2021-10-05 21:58:11 -0300

    Lista 4 - Exerc 1 (tratando exceção)

---
+++ Lista4-Exe1/src/Residencial.java
@@ -0,0 +1,23 @@
+
+
+import java.time.LocalDate;
+
+/**
+
+ */
+public class Residencial extends Telefone {
+    private boolean conexaoInternet;
+
+
+    public void setConexaoInternet(boolean conexaoInternet) {
+		this.conexaoInternet = conexaoInternet;
+	}
+
+	public boolean getConexaoInternet() {
+        return conexaoInternet;
+    }
+
+    public double calcularCustoFixoMensal(){
+        return 15.0;
+    }
+} 
